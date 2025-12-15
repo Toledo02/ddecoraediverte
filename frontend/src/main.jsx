dd8741e6f2cccad6d4672e6410a25b652314ff84
Gustavo Toledo <gustavotoledo02@gmail.com>
2025-12-15 20:29:53 -0300

    corrigindo importação do carrinho

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import theme from './theme'
-import { CartProvider } from '../contexts/CartContext'
+import { CartProvider } from './contexts/CartContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
