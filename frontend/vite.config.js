f11269f0b4e47d66586917b8340abe7a38296513
Gustavo Toledo <gustavotoledo02@gmail.com>
2025-12-14 21:51:04 -0300

    Criando o frontend com React + Vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Libera o acesso para o Docker expor a porta
+    port: 5173, 
+  }
+})
